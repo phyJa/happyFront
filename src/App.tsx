c29527209373f6652bb41613761e011d9f08c88f
Elias <eli.bueno9@gmail.com>
2020-10-12 15:34:37 -0300

    Making some tests with react and adding the first image to the project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
+interface titleProps {
+
+  text: string;
+
+}
+
+function Title(props: titleProps) {
+  return <h1> Here is title {props.text} </h1>;
+}
+
 function App() {
   return (
     <div className="App">
-      <h1> Hello world! </h1>
+      <Title text="1" />
+      <Title text="2" />
+      <Title text="3" />
     </div>
   );
 }
