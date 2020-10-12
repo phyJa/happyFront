88be74f14d08029b06a79e78e63b0b33e84a08ff
Elias <eli.bueno9@gmail.com>
2020-10-12 16:14:35 -0300

    Preparing the structure for the landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,45 @@
 import React from 'react';
 
+// Image
+import logoImg from './images/logo.svg';
+
+// CSS
+import './styles/pages/landing.css';
+
 function App() {
+
   return (
-    <div className="App">
-      <h1> Hello! </h1>
+
+    <div id="page-landing">
+
+      <div className="contentWrapper">
+
+        <img src={logoImg} alt="Happy" />
+
+        <main>
+
+          <h1> Leve felicidade para o mundo </h1>
+
+          <p> Visite orfanatos e mude o dia de muitas crianças. </p>
+
+        </main>
+
+        <div className="location">
+
+          <strong> São José dos Campos </strong>
+
+          <span> São Paulo </span>
+
+        </div>
+
+        <a href="#" className="enter-app"> > </a>
+
+      </div>
+
     </div>
+
   );
+
 }
 
 export default App;
