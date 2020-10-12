dd681b544b8295064cb19d2cc4df6bed1ce9ba1d
Elias <eli.bueno9@gmail.com>
2020-10-12 15:53:12 -0300

    Inserting a Google Font called 'Nunito'

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
 import React from 'react';
 
-interface titleProps {
-
-  text: string;
-
-}
-
-function Title(props: titleProps) {
-  return <h1> Here is title {props.text} </h1>;
-}
-
 function App() {
   return (
     <div className="App">
-      <Title text="1" />
-      <Title text="2" />
-      <Title text="3" />
+      <h1> Hello! </h1>
     </div>
   );
 }
