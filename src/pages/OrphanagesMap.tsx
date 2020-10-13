f360250ac75c79300d949611a667f9dcf9f94d74
Elias <eli.bueno9@gmail.com>
2020-10-13 14:46:33 -0300

    Structure of the OrphanagesMap done

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,10 +1,50 @@
 import React from 'react';
 
+// Image
+import mapMarkerImg from '../images/map-marker.svg';
+
+import { Link } from 'react-router-dom';
+
+// Icons
+import { FiPlus } from 'react-icons/fi';
+
 function OrphanagesMap() {
 
     return (
 
-        <h1> Hello </h1>
+        <div id="page-map">
+
+            <aside>
+
+                <header>
+
+                    <img src={mapMarkerImg} alt="Happy"/>
+
+                    <h2> Escolha um orfanato no mapa </h2>
+
+                    <p> Muitas crianças estão esperando a sua visita! </p>
+
+                </header>
+
+                <footer>
+
+                    <strong> São José dos Campos </strong>
+
+                    <span> São Paulo </span>
+
+                </footer>
+
+            </aside>
+
+            <div></div>
+
+            <Link to="" className="create-orphanage"> 
+            
+                <FiPlus size={ 32 } color="#FFF"/>
+
+            </Link>
+
+        </div>
 
     );
 
