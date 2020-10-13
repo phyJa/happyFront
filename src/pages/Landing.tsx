eeb85c1d4e14d86bfce500cefecc4c4788c12973
Elias <eli.bueno9@gmail.com>
2020-10-13 11:32:36 -0300

    Adding react-router-dom and its typing

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+// Image
+import logoImg from '../images/logo.svg';
+
+import { FiArrowRight } from 'react-icons/fi';
+
+// CSS
+import '../styles/pages/landing.css';
+
+function Landing() {
+
+    return (
+
+        <div id="page-landing">
+    
+          <div className="contentWrapper">
+    
+            <img src={logoImg} alt="Happy" />
+    
+            <main>
+    
+              <h1> Leve felicidade para o mundo </h1>
+    
+              <p> Visite orfanatos e mude o dia de muitas crianças. </p>
+    
+            </main>
+    
+            <div className="location">
+    
+              <strong> São José dos Campos </strong>
+    
+              <span> São Paulo </span>
+    
+            </div>
+    
+            <a href="#" className="enter-app"> 
+    
+              <FiArrowRight size={ 26 } color="rgba(0, 0, 0, 0.6)" /> 
+    
+            </a>
+    
+          </div>
+    
+        </div>
+    
+      );
+
+}
+
+export default Landing;
---
+++ src/routes.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+// Pages
+import Landing from './pages/Landing';
+import OrphanagesMap from './pages/OrphanagesMap';
+
+function Routes () {
+    
+    return(
+
+        <BrowserRouter>
+
+            <Route path="/" component={ Landing } />
+
+            <Route path="/app" component={ OrphanagesMap } />
+
+        </BrowserRouter>
+
+    );
+
+}
+
+export default Routes;
