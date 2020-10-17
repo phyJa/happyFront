f8c8760b7d8434dd07c2d0828d5129b0c3f8a243
Elias <eli.bueno9@gmail.com>
2020-10-17 09:47:19 -0300

    Adding the two new pages to the rroutes

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,7 +5,12 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 // Pages
 import Landing from './pages/Landing';
+
 import OrphanagesMap from './pages/OrphanagesMap';
+
+import Orphanage from './pages/Orphanage';
+
+import CreateOrphanage from './pages/CreateOrphanage';
 
 function Routes () {
     
@@ -18,6 +23,10 @@ function Routes () {
                 <Route path="/" component={ Landing } exact />
 
                 <Route path="/app" component={ OrphanagesMap } />
+
+                <Route path="/orphanages/create" component={ CreateOrphanage } />
+
+                <Route path="/orphanages/:id" component={ Orphanage } />
 
             </Switch>
 
