a9baa7d5a30d9df190754fdd16d1e4935b02d5db
Elias <eli.bueno9@gmail.com>
2020-10-17 18:56:24 -0300

    Using google maps link to show the  path to the orphanage

--- src/pages/Orphanage.tsx
+++ src/pages/Orphanage.tsx
@@ -133,7 +133,7 @@ export default function Orphanage() {
               </Map>
 
               <footer>
-                <a href="">Ver rotas no Google Maps</a>
+                <a href={`https://www.google.com/maps/dir/?api=1&destination=${orphanage.latitude},${orphanage.longitude}`}>Ver rotas no Google Maps</a>
               </footer>
             </div>
 
